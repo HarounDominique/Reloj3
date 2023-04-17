423a857018cbc8d5916bba14ca0586113dc1771f
HarounDominique <haroundominique@gmail.com>
2023-04-17 20:21:52 +0200

    Funcionando el reloj, pero revisar cómo y por qué lo hace

--- src/main/java/com/example/reloj3/HelloController.java
+++ src/main/java/com/example/reloj3/HelloController.java
@@ -1,21 +1,62 @@
 package com.example.reloj3;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
-import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class HelloController {
+    private boolean relojEncendido = false;
+    private Reloj reloj;
+
     @FXML
-    private Label reloj;
+    private Label relojLabel;
+
+    @FXML
+    private Button onOffButton;
 
     @FXML
     protected void onHelloButtonClick() {
-        Reloj r = new Reloj();
-        reloj.setText(r.);
-        timeLabel.setText(formattedTime);
+        if (relojEncendido) {
+            relojEncendido = false;
+            onOffButton.setText("Encender");
+            reloj.detener();
+        } else {
+            relojEncendido = true;
+            onOffButton.setText("Apagar");
+            reloj = new Reloj();
+            reloj.iniciar();
+        }
+    }
+
+    private class Reloj extends Thread {
+        private boolean ejecutando = false;
+
+        public void iniciar() {
+            ejecutando = true;
+            this.start();
+        }
+
+        public void detener() {
+            ejecutando = false;
+        }
+
+        @Override
+        public void run() {
+            while (ejecutando) {
+                try {
+                    sleep(1000);
+                    Platform.runLater(() -> {
+                        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+                        relojLabel.setText(LocalDateTime.now().format(formatter));
+                    });
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
--- src/main/java/com/example/reloj3/Reloj.java
+++ src/main/java/com/example/reloj3/Reloj.java
@@ -7,24 +7,40 @@ import java.io.Serializable;
 import java.text.SimpleDateFormat;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
+import java.util.TimerTask;
 
-class Reloj implements ActionListener, Serializable {
+class Reloj extends Thread implements ActionListener, Serializable {
 
 
     private boolean modo24horas;
     private boolean activar_alarma;
     private DateTimeFormatter formateador;
+    public String hora = "";
 
     public Reloj() {
-        Timer t = new Timer(1000, this);
-        t.start();
+
     }
+
+    public void arrancarReloj(){
+        //Timer t = new Timer(1000, this);
+        //t.start();
+
+    }
+    @Override
+    public void run(){
+        while(true){
+            Date d = new Date();
+            SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+            String formattedTime = sdf.format(d);
+            this.hora = formattedTime;
+        }
+    }
+
 
     public void actionPerformed(ActionEvent e) {
         Date d = new Date();
         SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
         String formattedTime = sdf.format(d);
-
+        this.hora = formattedTime;
     }
-
 }
